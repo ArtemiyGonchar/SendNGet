29da0af9256570610a52d7c4dd1c46446e863ba2
ArtemiyGonchar <artmiygonchar@gmail.com>
2024-04-17 19:57:54 +0300

    added new funtions for working with client connections

--- server/network.cpp
+++ server/network.cpp
@@ -4,6 +4,8 @@ Network::Network(QObject *parent)
     : QObject{parent}
 {
     m_server = new QTcpServer(this);
+
+    connect(m_server, &QTcpServer::newConnection, this, &Network::clientConnected);
 }
 
 bool Network::portStart(int port)
@@ -19,4 +21,30 @@ bool Network::portStart(int port)
     }
 
     return started;
+}
+
+void Network::clientConnected()
+{
+    QTcpSocket *client = m_server->nextPendingConnection();
+    QByteArray id = QUuid::createUuid().toByteArray();
+    m_clients[id] = client;
+
+    qDebug()<<("New client connected");
+
+    //connect(client, &QTcpSocket::readyRead, this, &Network::?);
+    connect(client, &QTcpSocket::disconnected, this, &Network::clientDisconnected);
+    emit onClientArrived(id);
+}
+
+void Network::clientDisconnected()
+{
+    QTcpSocket *client = (QTcpSocket*)sender();
+    for(QByteArray id : m_clients.keys()){
+        if (m_clients[id] == client){
+            qDebug()<<"Client disconnected";
+            emit onClientDisconnected(id);
+            m_clients.remove(id);
+        }
+    }
+
 }
--- server/network.h
+++ server/network.h
@@ -5,6 +5,7 @@
 #include <QTcpServer>
 #include <QTcpSocket>
 #include <QDebug>
+#include <QUuid>
 
 class Network : public QObject
 {
@@ -15,8 +16,15 @@ public:
     bool portStart(int port);
 private:
     QTcpServer *m_server;
+    QMap<QByteArray, QTcpSocket*> m_clients;
 
 signals:
+    void onClientArrived(QByteArray id);
+    void onClientDisconnected(QByteArray id);
+
+private slots:
+    void clientConnected();
+    void clientDisconnected();
 };
 
 #endif // NETWORK_H
