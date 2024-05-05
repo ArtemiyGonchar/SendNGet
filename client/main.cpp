a22b982c70e38e1b594bd3ef81839ef2be53bc41
ArtemiyGonchar <artmiygonchar@gmail.com>
2024-05-05 22:49:38 +0300

    added stylesheet

--- client/main.cpp
+++ client/main.cpp
@@ -1,10 +1,23 @@
 #include "client.h"
-
+#include <QFile>
 #include <QApplication>
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QString path = QDir::currentPath();
+    QString choppedPath;
+    for (int i = 0; i < path.indexOf("SendNGet") + 8; i++){
+        choppedPath += path[i];
+    }
+    choppedPath += "/client/Adaptic.qss";
+
+    QFile styleSheetFile(choppedPath);
+    QDir::currentPath();
+    styleSheetFile.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(styleSheetFile.readAll());
+    a.setStyleSheet(styleSheet);
+
     Client w;
     w.show();
     return a.exec();
